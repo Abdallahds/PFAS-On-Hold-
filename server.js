f7f3eb1ca7581a8f3523a954a33a24023b98f04d
Abdallah <abdallahalazzam3@gmail.com>
2022-04-21 13:33:48 -0400

    add admin user and login feature

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,6 +11,20 @@ app.use(express.static('public'));
 app.listen(3000, () => {
     console.log("the server is on using port 3000");
 })
+
+/////////////////////////dataBase/////////////////
+
+mongoose.connect("mongodb://localhost:27017/myGym", { useUnifiedTopology: true, useNewUrlParser: true })
+
+const userSchema = mongoose.Schema({
+    userName: String,
+    password: String,
+    role: String
+})
+
+const userModle = mongoose.model("user", userSchema);
+
+/////////////////////////get//////////////////////
 
 app.get("/", (req, res) => {
     res.render(__dirname + "/pages/index")
@@ -22,9 +37,14 @@ app.get("/admin", (req, res) => {
     res.render(__dirname + "/pages/admin")
 })
 
+
+///////////////////////post////////////////////////
+
 app.post("/login", (req, res) => {
-    console.log(req.body.userName)
-    console.log(req.body.password);
-    res.render(__dirname + "/pages/login")
+    userModle.findOne({ userName: req.body.userName }, (err, doc) => {
+        if (doc.password == req.body.password)
+            if (doc.role == "1")
+                res.render(__dirname + "/pages/admin")
+    })
 })
 
